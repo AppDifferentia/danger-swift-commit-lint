aba3d92c68210052c42072f1fce7866130e168a2
Wei Sun <weisunOW@users.noreply@github.com>
2021-09-06 22:56:37 +1000

    [Project] Initial Project Setup
    - project structure
    - dependencies
    - CI
    - Add `.swiftlint.yml`

---
+++ Dangerfile.swift
@@ -0,0 +1,39 @@
+import Danger
+import Foundation
+
+let danger = Danger()
+
+if danger.git.createdFiles.count + danger.git.modifiedFiles.count - danger.git.deletedFiles.count > 300 {
+    warn("Big PR, try to keep changes smaller if you can")
+}
+
+let swiftFilesWithCopyright = danger.git.createdFiles.filter {
+    $0.fileType == .swift && danger.utils.readFile($0).contains("//  Created by")
+}
+
+if swiftFilesWithCopyright.isEmpty == false {
+    let files = swiftFilesWithCopyright.joined(separator: ", ")
+    warn("In DangerSwiftCommitLint we don't include copyright headers, found them in: \(files)")
+}
+
+let filesToLint = (danger.git.modifiedFiles + danger.git.createdFiles)
+    .filter { URL(fileURLWithPath: $0).pathExtension == "swift" }
+
+SwiftLint.lint(.files(filesToLint), inline: true)
+
+// Only run GitHub related checks when GitHub dsl is available. This allows `danger-swift local` to work.
+guard let github = danger.github else {
+    warn("Unable to parse GitHub DSL response, the GitHub related checks will be skipped. (i.e. running `danger-swift local`).")
+    exit(0)
+}
+
+// These checks only happen on a PR
+let foundWIPMessageInTitle = github.pullRequest.title.contains("Work In Progress")
+    || github.pullRequest.title.contains("WIP")
+let foundWIPLabel = github.issue.labels.contains {
+    $0.name.contains("Work In Progress")
+}
+
+if foundWIPMessageInTitle || foundWIPLabel {
+    warn("PR is classed as Work in Progress")
+}
---
+++ Package.swift
@@ -0,0 +1,50 @@
+// swift-tools-version:5.4
+
+import PackageDescription
+
+let package = Package(
+    name: "DangerSwiftCommitLint",
+    products: [
+        .library(name: "DangerSwiftCommitLint", targets: ["DangerSwiftCommitLint"]),
+        .library(name: "DangerDeps", type: .dynamic, targets: ["DangerDependencies"]), // dev
+    ],
+    dependencies: [
+        .package(name: "danger-swift", url: "https://github.com/danger/swift", from: "3.0.0"),
+        // Dev dependencies.
+        .package(url: "https://github.com/shibapm/Komondor", from: "1.0.0"), // dev
+        .package(url: "https://github.com/shibapm/Rocket", from: "1.2.0"), // dev
+        .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.48.0"), // dev
+        .package(url: "https://github.com/realm/SwiftLint", from: "0.43.0"), // dev
+    ],
+    targets: [
+        .target(name: "DangerDependencies", dependencies: [.product(name: "Danger", package: "danger-swift")]), // dev
+        .target(name: "DangerSwiftCommitLint", dependencies: [.product(name: "Danger", package: "danger-swift")]),
+        .testTarget(
+            name: "DangerSwiftCommitLintTests",
+            dependencies: [
+                "DangerSwiftCommitLint",
+                .product(name: "DangerFixtures", package: "danger-swift"),
+            ]
+        ),
+    ]
+)
+
+#if canImport(PackageConfig)
+    import PackageConfig
+
+    let config = PackageConfiguration([
+        "komondor": [
+            "pre-commit": [
+                "swift test --generate-linuxmain",
+                "swift run swiftformat .",
+                "swift run swiftlint autocorrect --path Sources/",
+                "git add .",
+            ],
+        ],
+        "rocket": [
+            "after": [
+                "push",
+            ],
+        ],
+    ]).write()
+#endif
