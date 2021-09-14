dda4edd69c66c95673b3d5944ed034f760e31a6e
Wei Sun <weisunOW@users.noreply@github.com>
2021-09-14 15:11:11 +1000

    Version v0.1.0

--- Package.swift
+++ Package.swift
@@ -6,26 +6,26 @@ let package = Package(
     name: "DangerSwiftCommitLint",
     products: [
         .library(name: "DangerSwiftCommitLint", targets: ["DangerSwiftCommitLint"]),
-        .library(name: "DangerDeps", type: .dynamic, targets: ["DangerDependencies"]), // dev
+//        .library(name: "DangerDeps", type: .dynamic, targets: ["DangerDependencies"]), // dev
     ],
     dependencies: [
         .package(name: "danger-swift", url: "https://github.com/danger/swift", from: "3.0.0"),
         // Dev dependencies.
-        .package(name: "DangerSwiftCoverage", url: "https://github.com/f-meloni/danger-swift-coverage", .upToNextMajor(from: "1.2.1")), // dev
-        .package(url: "https://github.com/shibapm/Komondor", from: "1.0.0"), // dev
-        .package(url: "https://github.com/shibapm/Rocket", from: "1.2.0"), // dev
-        .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.48.0"), // dev
-        .package(url: "https://github.com/realm/SwiftLint", from: "0.43.0"), // dev
+//        .package(name: "DangerSwiftCoverage", url: "https://github.com/f-meloni/danger-swift-coverage", .upToNextMajor(from: "1.2.1")), // dev
+//        .package(url: "https://github.com/shibapm/Komondor", from: "1.0.0"), // dev
+//        .package(url: "https://github.com/shibapm/Rocket", from: "1.2.0"), // dev
+//        .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.48.0"), // dev
+//        .package(url: "https://github.com/realm/SwiftLint", from: "0.43.0"), // dev
     ],
     targets: [
-        .target( // dev
-            name: "DangerDependencies", // dev
-            dependencies: [ // dev
-                .product(name: "Danger", package: "danger-swift"), // dev
-                "DangerSwiftCoverage", // dev
-                "DangerSwiftCommitLint", // dev
-            ] // dev
-        ), // dev
+//        .target( // dev
+//            name: "DangerDependencies", // dev
+//            dependencies: [ // dev
+//                .product(name: "Danger", package: "danger-swift"), // dev
+//                "DangerSwiftCoverage", // dev
+//                "DangerSwiftCommitLint", // dev
+//            ] // dev
+//        ), // dev
         .target(name: "DangerSwiftCommitLint", dependencies: [.product(name: "Danger", package: "danger-swift")]),
         .testTarget(
             name: "DangerSwiftCommitLintTests",
